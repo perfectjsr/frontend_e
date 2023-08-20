f687196cc14532646e3c5b6e9e58b32dd74eb085
sanjay96696@gmail.com <83408472+perfectjsr@users.noreply.github.com>
2023-08-20 23:19:12 +0530

    Bootstrap

--- React/data.js
+++ React/data.js
@@ -19,15 +19,123 @@ const data = [
     },  
     {
         id : 4,
-        name : "Baghban",
-        desciption : "An elderly couple wish their children to care for them in their old age. But their children see and treat them as a burden, and they must struggle to regain their worth and dignity to themselves and others.",
-        year : 2003
-    },  
+        name : "Kabhi Khushi Kabhie Gham",
+        desciption : "After marrying a poor woman, rich Rahul is disowned by his father and moves to London to build a new life. Years later, his now-grown younger brother Rohan embarks on a mission to bring Rahul back home and reunite the family.",
+        year : 2001
+    },
     {
         id : 5,
-        name : "Baghban",
+        name : "Drishyam",
+        desciption : "A man goes to extreme lengths to save his family from punishment after the family commits an accidental crime.",
+        year : 2013
+    }, 
+    {
+        id : 6,
+        name : "Dharm",
+        desciption : "A very timely piece (don't let the year fool you) about religious differences in India. I dare you not to cry.",
+        year : 1945
+    }, 
+    {
+        id : 7,
+        name : "Thirst",
+        desciption : "A poet's angst, not seeking recognition so much as just trying to make a living. Brilliantly presented, don't be afraid of the poetry, it's magical.",
+        year : 1957
+    }, 
+    {
+        id : 8,
+        name : "Raincoat",
+        desciption : "All he wanted was to see her just once. Manu was from a village in the backwoods. Neeru was the girl next door, his lost love.",
+        year : 2004
+    }, 
+    {
+        id : 9,
+        name : "Devda",
+        desciption : "After his wealthy family prohibits him from marrying the woman he is in love with, Devdas Mukherjee's life spirals further and further out of control as he takes up alcohol and a life of vice to numb the pain.",
+        year : 2002
+    }, 
+    {
+        id : 10,
+        name : "Kaksparsh",
+        desciption : "Uma loses her husband at a very young age and faces many hardships. The only support she gets is from her brother-in-law.",
+        year : 2012
+    }, 
+    {
+        id : 11,
+        name : "Bahubali: The Beginning",
+        desciption : "A child from the Mahishmati kingdom is raised by tribal people and one day learns about his royal heritage, his father's bravery in battle and a mission to overthrow the incumbent ruler.",
+        year : 2015
+    }, 
+    {
+        id : 12,
+        name : "Jodha Akbar",
+        desciption : "A sixteenth century love story about a marriage of alliance that gave birth to true love between a great Mughal emperor, Akbar, and a Rajput princess, Jodha.",
+        year : 2008
+    }, 
+    {
+        id : 13,
+        name : "Mughal-E-Azam",
+        desciption : "A 16th century prince falls in love with a court dancer and battles with his emperor father.",
+        year : 1960
+    }, 
+    {
+        id : 14,
+        name : "OMG",
+        desciption : "A shopkeeper takes God to court when his shop is destroyed by an earthquake.",
+        year : 2012
+    }, 
+    {
+        id : 15,
+        name : "Mohabbatein",
+        desciption : "A film about the battle between love and fear, between two stubborn men and their opposing beliefs and the outcome of the love stories of 3 couples.",
+        year : 2000
+    }, 
+    {
+        id : 16,
+        name : "Rann",
+        desciption : "The revered chief of a news agency tries to maintain journalistic integrity as his bureau and a rival agency vie for viewers following a terrorist attack.",
+        year : 2010
+    }, 
+    {
+        id : 17,
+        name : "Dabangg",
+        desciption : "A corrupt police officer faces challenges from his family, gangsters and politicians.",
+        year : 2010
+    }, 
+    {
+        id : 18,
+        name : "3 Idiots",
+        desciption : "Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them 'idiots'.",
+        year : 2009
+    }, 
+    {
+        id : 19,
+        name : "Toilet: Ek Prem Katha",
         desciption : "An elderly couple wish their children to care for them in their old age. But their children see and treat them as a burden, and they must struggle to regain their worth and dignity to themselves and others.",
-        year : 2003
+        year : 2017
+    }, 
+    {
+        id : 20,
+        name : "Kesari",
+        desciption : "Based on an incredible true story of the Battle of Saragarhi in which an army of 21 Sikhs fought against 10,000 Afghans in 1897.",
+        year : 2019
+    }, 
+    {
+        id : 21,
+        name : "Padman",
+        desciption : "Upon realizing the extent to which women are affected by their menses, a man sets out to create a sanitary pad machine and to provide inexpensive sanitary pads to the women of rural India.",
+        year : 2018
+    }, 
+    {
+        id : 22,
+        name : "Cycle",
+        desciption : "A post-Independence era story, Cycle is about 'Keshav''s intense love for his cycle which gets stolen by two thieves. Will Keshav ever find his cycle back?",
+        year : 2018
+    }, 
+    {
+        id : 23,
+        name : "Gulabjamun",
+        desciption : "Aditya is a young Maharashtrian man from London who comes to Pune to find a someone who would teach him traditional Maharashtrian cooking. His aim is to open a Marathi Gourmet Restaurant in London.",
+        year : 2018
     }, 
      
 ]
