800f9dec216160a4f4a494f294ffe8568b6bd68c
sanjay96696@gmail.com <83408472+perfectjsr@users.noreply.github.com>
2023-08-25 17:09:01 +0530

    Hooks Day-1

--- React2/myapp/src/components/ImageCard.jsx
+++ React2/myapp/src/components/ImageCard.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+
 const ImageCard = (props) => {
+  const [Count,setCount] = useState(0);
+  const Handleclick =() => {
+    setCount(Count + 1)
+    console.log("button is clicked", Count);
+  }
+
   return (
     <div>
     <Card style={{ width: '18rem' ,height: '45rem' }}>
@@ -12,7 +20,8 @@ const ImageCard = (props) => {
       <Card.Text>
         {props.desc}
       </Card.Text>
-      <Button className='ImgButton' variant="primary">Get Details</Button>
+      <Button className='ImgButton' onClick={Handleclick} variant="primary">Book Ticket - {Count}</Button>
+      
     </Card.Body>
   </Card>
     </div>
