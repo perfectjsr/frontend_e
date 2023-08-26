2033f2551808bef449707410a2d3a72942a82479
sanjay96696@gmail.com <83408472+perfectjsr@users.noreply.github.com>
2023-08-26 14:51:37 +0530

    Counter React App

---
+++ React2/counterapp/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import MyCard from './components/MyCard';
+
+function App() {
+  return (
+    <MyCard/>
+  );
+}
+
+export default App;
---
+++ React2/counterapp/src/components/MyCard.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+
+
+const MyCard = () => {
+    const [Count, setCount] = useState(0);
+    const HandleclickI = () => {
+        setCount(Count + 1)
+        console.log("button is clicked", Count);
+    }
+    const HandleclickD = () => {
+        setCount(Count - 1)
+        if(Count>0){
+            console.log("button is clicked", Count);
+        }
+        else{
+            alert("Minimum click amount reached i.e. 0")
+            setCount(0)
+            console.log("Minimum click amount reached i.e. 0", Count);
+        }
+    }
+
+    return (
+        <div>
+            <Card className='card' >
+                <h1>{Count}</h1>
+                <Button onClick={HandleclickI}>Increase</Button>
+                <Button onClick={HandleclickD}>Decrease</Button>
+
+            </Card>
+        </div>
+    )
+}
+
+export default MyCard
